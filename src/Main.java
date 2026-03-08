dbe6460f7823bdee6a4dce77361972b9be6132b3
patrickfitzgerald <fitzgepc@mail.uc.edu>
2026-03-08 15:51:19 -0400

    Final Commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,98 @@
+import java.util.Random;
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Random gen = new Random();
+
+        Scanner in = new Scanner(System.in);
+
+        String playAgain = "";
+        boolean done1 = false;
+        boolean done2 = false;
+        boolean answer = false;
+
+
+        do {
+            int die1 = 0;
+            int die2 = 1;
+            int dieRoll = die1 + die2;
+            int thePoint= 0;
+
+            die1 = gen.nextInt(6) + 1;
+            die2 = gen.nextInt(6) + 1;
+            dieRoll = die1 + die2;
+
+
+            System.out.println("Roll: " + die1 + " + " + die2 + " = " + dieRoll );
+
+            if (dieRoll == 2 || dieRoll == 3 || die2 == 12){
+
+                System.out.println("Craps! You Lose!");
+
+            } else if (dieRoll == 7 || dieRoll == 11) {
+                System.out.println("Natural! You Win!");
+
+            } else {
+
+                thePoint = dieRoll;
+                System.out.println("The point is " + thePoint);
+
+
+                do {
+                    die1 = gen.nextInt(6) + 1;
+                    die2 = gen.nextInt(6) + 1;
+                    dieRoll = die1 + die2;
+                    System.out.println("Trying for the Point! The Point is: " + thePoint);
+                    System.out.println("Roll: " + die1 + " + " + die2 + " = " + dieRoll );
+
+                    if (dieRoll == 7){
+
+                        System.out.println("You rolled a 7. You Lose!");
+                        done2 = true;
+
+                    } else if (dieRoll == thePoint) {
+
+                        System.out.println("You made the point! You Win!");
+                        done2 = true;
+                    }
+                    else {
+                        System.out.println("You did not make the point! Roll Again!");
+                        done2 = false;
+                    }
+
+                }
+                while (!done2);
+
+            }
+
+            do {
+                System.out.println("Would you like to play again[Y/N]?");
+                playAgain = in.nextLine();
+                if (playAgain.equalsIgnoreCase("Y"))
+                {
+                    answer = true;
+                    done1 = false;
+                }
+                else if (playAgain.equalsIgnoreCase("N"))
+                {
+                    answer = true;
+                    done1 = true;
+                }
+                else
+                {
+                    System.out.println("Inavlid answer! Please choose [Y/N].");
+                }
+
+            } while (!answer);
+
+
         }
+        while (!done1) ;
+
+
     }
 }
